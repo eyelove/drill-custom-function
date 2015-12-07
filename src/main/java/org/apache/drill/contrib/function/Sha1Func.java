81516f6b259d357be181f3ec4555998ffcd63ad3
KimDeokJung <djkim@onnuridmc.com>
2015-12-07 19:45:13 +0900

    class name change

--- src/main/java/org/apache/drill/contrib/function/Sha1Func.java
+++ src/main/java/org/apache/drill/contrib/function/Sha1Func.java
@@ -1,5 +1,7 @@
 package org.apache.drill.contrib.function;
 
+import java.security.*;
+
 import javax.inject.Inject;
 
 import org.apache.drill.exec.expr.DrillSimpleFunc;
@@ -16,7 +18,7 @@ import io.netty.buffer.DrillBuf;
         scope = FunctionTemplate.FunctionScope.SIMPLE,
         nulls = FunctionTemplate.NullHandling.NULL_IF_NULL
 )
-public class Sha1Func implements DrillSimpleFunc  {
+public class SHA1Func implements DrillSimpleFunc  {
 
 	@Param
     NullableVarCharHolder input;
@@ -28,25 +30,12 @@ public class Sha1Func implements DrillSimpleFunc  {
     DrillBuf buffer;
     
 	public void setup() {
-		
 	}
 
 	public void eval() {
         String stringValue = org.apache.drill.exec.expr.fn.impl.StringFunctionHelpers.toStringFromUTF8(input.start, input.end, input.buffer);
 
-        String outputValue = "";
-		try {
-			java.security.MessageDigest mDigest = java.security.MessageDigest.getInstance("SHA1");
-			byte[] result = mDigest.digest(stringValue.getBytes());
-			StringBuffer sb = new StringBuffer();
-			for (int i = 0; i < result.length; i++) {
-				sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
-			}
-			
-			outputValue = sb.toString();
-	    } catch (java.security.NoSuchAlgorithmException e) {
-	        e.printStackTrace();
-	    }
+        String outputValue = encSha1(stringValue);
 
         // put the output value in the out buffer
         out.buffer = buffer;
@@ -54,5 +43,25 @@ public class Sha1Func implements DrillSimpleFunc  {
         out.end = outputValue.getBytes().length;
         buffer.setBytes(0, outputValue.getBytes());
 	}
+
+	private static String encSha1(String input) {
+		try {
+			MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+			byte[] result = mDigest.digest(input.getBytes());
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < result.length; i++) {
+				sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			
+			return sb.toString();
+	    } catch (NoSuchAlgorithmException e) {
+	        e.printStackTrace();
+	    }
+	    return "";
+    }
 	
+//     public static void main(String args[]) {
+//    	 String stringValue = "abcde";
+//         System.out.println(encSha1(stringValue));
+//     }
 }
